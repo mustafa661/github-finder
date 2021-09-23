60cce8eaa82a934d0f99899bb69054a9204caa7c
mustafa <mustafashabbir804@gmail.com>
2021-09-24 00:32:46 +0500

    Update GithubState.js

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+
+const About = () => {
+    return (
+        <Fragment>
+            <h1>About the Github Finder App</h1>
+            <p>The purpose of this app is to allow users to search Github Users </p>
+        </Fragment>    
+    )
+}
+
+export default About
